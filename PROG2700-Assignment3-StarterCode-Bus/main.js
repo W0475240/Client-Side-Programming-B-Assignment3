2ac5087c05c3f66a0162df839b575d58cf9f604c
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-10-25 16:34:33 -0300

    fetch data

--- PROG2700-Assignment3-StarterCode-Bus/main.js
+++ PROG2700-Assignment3-StarterCode-Bus/main.js
@@ -4,6 +4,7 @@
     //create map in leaflet and tie it to the div called 'theMap'
     let map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
+    // Adding a title layer use of the openstreetmap data
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
@@ -17,31 +18,11 @@
         // an asynchronous function to update bus data
         async function update_bus_data()
         {
+            // Fetch bus data from a remote API.
             var raw_data = await fetch("https://prog2700.onrender.com/hrmbuses");
             var data = await raw_data.json();
+            // Filter the data to select only routes with a routeId less than or equal to 10.
             var selected_routes = data.entity.filter((x) => x.vehicle.trip.routeId <= 10);
 
-            map.eachLayer(function (layer) 
-            {
-                if (layer instanceof L.Marker) 
-                {
-                    map.removeLayer(layer);
-                }
-            });
-
-            // Add markers for the selected bus routes with custom icons
-            selected_routes.map((route) =>
-            {
-                var geojsonFeature = 
-                {
-                    "type": "Feature",
-                    "properties":{},
-                    "geometry": 
-                    {
-                        "type": "Point",
-                        "coordinates": [route.vehicle.position.longitude, route.vehicle.position.latitude]
-                    }
-
-                }
-
+           
 })()
