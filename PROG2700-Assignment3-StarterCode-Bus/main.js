427a49db816de69599fc5b794b961724c4d319d8
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-10-30 20:55:52 -0300

    complete

--- PROG2700-Assignment3-StarterCode-Bus/main.js
+++ PROG2700-Assignment3-StarterCode-Bus/main.js
@@ -4,7 +4,6 @@
     //create map in leaflet and tie it to the div called 'theMap'
     let map = L.map('theMap').setView([44.650627, -63.597140], 14);
 
-    // Adding a title layer use of the openstreetmap data
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
@@ -15,14 +14,51 @@
 
 
 
-        // an asynchronous function to update bus data
+
         async function update_bus_data()
         {
-            // Fetch bus data from a remote API.
             var raw_data = await fetch("https://prog2700.onrender.com/hrmbuses");
             var data = await raw_data.json();
-            // Filter the data to select only routes with a routeId less than or equal to 10.
             var selected_routes = data.entity.filter((x) => x.vehicle.trip.routeId <= 10);
 
-           
+            map.eachLayer(function (layer) 
+            {
+                if (layer instanceof L.Marker) 
+                {
+                    map.removeLayer(layer);
+                }
+            });
+
+            selected_routes.map((route) =>
+            {
+                var geojsonFeature = 
+                {
+                    "type": "Feature",
+                    "properties":{},
+                    "geometry": 
+                    {
+                        "type": "Point",
+                        "coordinates": [route.vehicle.position.longitude, route.vehicle.position.latitude]
+                    }
+
+                }
+
+                var custom_icon = L.icon({
+                    iconUrl: 'bus.png',
+                    // the icon is 100 * 102
+                    iconSize: [50,51],
+                    iconAnchor: [25,25.5],
+                    popupAnchor: [0,-25.5]
+                    });
+
+                L.geoJSON(geojsonFeature,{ pointToLayer: function(feature,latlng){return L.marker(latlng,{icon: custom_icon,rotationAngle:route.vehicle.position.bearing})}}).addTo(map).bindPopup("Vehicle ID : " + route.vehicle.vehicle.id + "<br>Route ID : " + route.vehicle.trip.routeId + "<br>Speed : " + route.vehicle.position.speed + "<br>Heading : " + route.vehicle.position.bearing);
+                return route;
+            });
+            console.log(selected_routes);
+            await new Promise(r => setTimeout(r, 10000));
+            update_bus_data();
+
+        }
+
+        update_bus_data();
 })()
